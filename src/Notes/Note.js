ee9e4f52616261b6d506e7722bed9da525de7a54
Boboe16 <114482350+Boboe16@users.noreply.github.com>
2023-05-02 07:40:55 -0700

    Add files via upload

--- src/Notes/Note.js
+++ src/Notes/Note.js
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from 'react'
 import {Link} from 'react-router-dom';
-import setting_icon from './1008927-200.png'
 import add_icon from './Green-Add-Button-PNG.png'
 import delete_icon from './x-square-close-delete-icon-512x512-jzeyu3ut.png'
 import './NoteStyle.css'
@@ -9,15 +8,14 @@ function Header() {
   return (
     <div id='Header'>
       <h1>Notepud</h1>
-      <img src={setting_icon} alt='setting_icon'/>
     </div>
   )
 }
 
 function Content() {
   let [showAdd, setShowAdd] = useState(false)
-
-  let [showView, setShowView] = useState(false)
+  
+  let [showView, setShowView] = useState(false) 
 
   let [re_render ,setRe_RenderContent] = useState(0)
  
@@ -31,8 +29,8 @@ function Content() {
 
   async function save(originalNote) { 
     /* The save function saves a note to local storage. It first gets the values of the title and content inputs from the form. If the title input is empty, the function doesn't save anything. If the client is editing an existing note, the function removes the old note from local storage by removing the key-value pairs for the old note's title and content. Then, it sets the new note's title and content as new key-value pairs in local storage. This function allows the client to save a new note or update an existing note in local storage. */
-    let Title = document.querySelector('#Title').value
-    let Content = document.querySelector('#Note-Content').value
+    let Title = document.querySelector('#Title-Input').value
+    let Content = document.querySelector('#Note-Content-Input').value
 
     if (Title === '') {
       return
@@ -58,6 +56,7 @@ function Content() {
 
   async function view(noteTitle) {
     /*The view function get the title of the selected note and set the state of Title, Content, and originalContent using it. It then set the state showView to false then true, after its true the showView ternary operator is executed and addViewEdit component is rendered with props Title, Content, and the save function with originalNote as argument.*/
+
     setOriginalNote(noteTitle)
     setTitle(noteTitle)
     let array = []
@@ -89,13 +88,28 @@ function Content() {
     });
   }, []);
 
+  function hideAndShow(id, id2, tagName, visibility) {
+    document.getElementById(`${id}`).style.visibility = `${visibility}`
+    document.getElementById(`${id2}`).style.visibility = `${visibility}`
+    document.getElementsByTagName(`${tagName}`)[0].style.overflowY = `${visibility}`
+  }
+
+  try {
+    showView ? hideAndShow('Navbar', 'Add', 'body', 'hidden') : hideAndShow('Navbar', 'body', 'visible')   
+  } catch(error) { console.log(error) } 
+
+  try {
+    showAdd ? hideAndShow('Navbar', 'Add', 'body', 'hidden') : hideAndShow('Navbar', 'body', 'visible')
+  } catch(error) { console.log(error) } 
+
   return (
+    <>
     <div id='Content'>
       <ul>
        {filteredKeys.map(noteTitle => {
         return (
           <>
-          <div className='Notes'>
+          <div className='Note'>
             <li className='Note-Title' onClick={() => view(noteTitle)} key={noteTitle}>{noteTitle}</li>
           </div>
           <img className='Note-Delete' src={delete_icon} onClick={() => deleteNote(noteTitle)} alt='delete-icon'/>
@@ -110,21 +124,21 @@ function Content() {
       }} />
       {showAdd && <AddViewEdit save={save} />}
       {showView && <AddViewEdit save={() => save(originalNote)} pee={title} poo={content} />}
-      
       <ul id='Navbar'>
         <li><Link to={"/"}>Notes</Link></li>
         <li><Link to={"/Todos"}>To-dos</Link></li>
       </ul>
     </div>
+    </>
   )
 }
 
 function AddViewEdit(props)  {
   return (
       <form id='Add_View_Edit' onSubmit={props.save}>
-        <input type="text" id="Title" placeholder="Title" defaultValue={props.pee}/>
-        <textarea id="Note-Content" cols="25" rows="2" placeholder="Note something down" defaultValue={props.poo}></textarea>
-        <input type="submit" id="Save" placeholder="Save" value='Save'/>
+        <input type="text" id="Title-Input" placeholder="Title" defaultValue={props.pee}/>
+        <textarea id="Note-Content-Input" cols="25" rows="2" placeholder="Note something down" defaultValue={props.poo}></textarea>
+        <input type="submit" id="Save-Note" placeholder="Save" value='Save'/>
       </form>
   )
 }
